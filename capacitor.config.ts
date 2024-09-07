e78dd5f8f8683af0a1257e5c91a2b68a455988bf
Jiang Xiachen <102943264+Xiachen-Jiang@users.noreply.github.com>
2024-09-07 13:57:31 +0930

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'connect-community',
+  webDir: 'dist'
+};
+
+export default config;
